01283284bbdfe06003f6262ed746b94088f0b3df
Fane Fonseka <fane247@gmail.com>
2017-06-26 16:57:01 +0100

    added first round swap hand cards with faceup cards. can't swap more than once though...

---
+++ js/deck.js
@@ -0,0 +1,128 @@
+var testDeck = {
+
+	cards: {
+
+		h2:{  
+		  name:'h2',
+		  suit:'h',
+		  value:2
+		},
+		h3:{  
+		  name:'h3',
+		  suit:'h',
+		  value:3
+		},
+		h4:{  
+		  name:'h4',
+		  suit:'h',
+		  value:4
+		},
+		h5:{  
+		  name:'h5',
+		  suit:'h',
+		  value:5
+		},
+		h6:{  
+		  name:'h6',
+		  suit:'h',
+		  value:6
+		},
+		h7:{  
+		  name:'h7',
+		  suit:'h',
+		  value:7
+		},
+		h8:{  
+		  name:'h8',
+		  suit:'h',
+		  value:8
+		},
+		h9:{  
+		  name:'h9',
+		  suit:'h',
+		  value:9
+		},
+		h10:{  
+		  name:'h10',
+		  suit:'h',
+		  value:10
+		},
+		hJ:{  
+		  name:'hJ',
+		  suit:'h',
+		  value:11
+		},
+		hQ:{  
+		  name:'hQ',
+		  suit:'h',
+		  value:12
+		},
+		hK:{  
+		  name:'hK',
+		  suit:'h',
+		  value:13
+		},
+		hA:{  
+		  name:'hA',
+		  suit:'h',
+		  value:14
+		}
+	}
+}
+
+var deck = [
+
+	{name: 'h2',suit: 'h',rank: '2',value: 2},
+	{name: 'h3',suit: 'h',rank: '3',value: 3},
+	{name: 'h4',suit: 'h',rank: '4',value: 4},
+	{name: 'h5',suit: 'h',rank: '5',value: 5},
+	{name: 'h6',suit: 'h',rank: '6',value: 6},
+	{name: 'h7',suit: 'h',rank: '7',value: 7},
+	{name: 'h8',suit: 'h',rank: '8',value: 8},
+	{name: 'h9',suit: 'h',rank: '9',value: 9},
+	{name: 'h10',suit: 'h',rank: '10',value: 10},
+	{name: 'hJ',suit: 'h',rank: 'J',value: 11},
+	{name: 'hQ',suit: 'h',rank: 'Q',value: 12},
+	{name: 'hK',suit: 'h',rank: 'K',value: 13},
+	{name: 'hA',suit: 'h',rank: 'A',value: 14},
+	{name: 'c2',suit: 'c',rank: '2',value: 2},
+	{name: 'c3',suit: 'c',rank: '3',value: 3},
+	{name: 'c4',suit: 'c',rank: '4',value: 4},
+	{name: 'c5',suit: 'c',rank: '5',value: 5},
+	{name: 'c6',suit: 'c',rank: '6',value: 6},
+	{name: 'c7',suit: 'c',rank: '7',value: 7},
+	{name: 'c8',suit: 'c',rank: '8',value: 8},
+	{name: 'c9',suit: 'c',rank: '9',value: 9},
+	{name: 'c10',suit: 'c',rank: '10',value: 10},
+	{name: 'cJ',suit: 'c',rank: 'J',value: 11},
+	{name: 'cQ',suit: 'c',rank: 'Q',value: 12},
+	{name: 'cK',suit: 'c',rank: 'K',value: 13},
+	{name: 'cA',suit: 'c',rank: 'A',value: 14},
+	{name: 's2',suit: 's',rank: '2',value: 2},
+	{name: 's3',suit: 's',rank: '3',value: 3},
+	{name: 's4',suit: 's',rank: '4',value: 4},
+	{name: 's5',suit: 's',rank: '5',value: 5},
+	{name: 's6',suit: 's',rank: '6',value: 6},
+	{name: 's7',suit: 's',rank: '7',value: 7},
+	{name: 's8',suit: 's',rank: '8',value: 8},
+	{name: 's9',suit: 's',rank: '9',value: 9},
+	{name: 's10',suit: 's',rank: '10',value: 10},
+	{name: 'sJ',suit: 's',rank: 'J',value: 11},
+	{name: 'sQ',suit: 's',rank: 'Q',value: 12},
+	{name: 'sK',suit: 's',rank: 'K',value: 13},
+	{name: 'sA',suit: 's',rank: 'A',value: 14},
+	{name: 'd2',suit: 'd',rank: '2',value: 2},
+	{name: 'd3',suit: 'd',rank: '3',value: 3},
+	{name: 'd4',suit: 'd',rank: '4',value: 4},
+	{name: 'd5',suit: 'd',rank: '5',value: 5},
+	{name: 'd6',suit: 'd',rank: '6',value: 6},
+	{name: 'd7',suit: 'd',rank: '7',value: 7},
+	{name: 'd8',suit: 'd',rank: '8',value: 8},
+	{name: 'd9',suit: 'd',rank: '9',value: 9},
+	{name: 'd10',suit: 'd',rank: '10',value: 10},
+	{name: 'dJ',suit: 'd',rank: 'J',value: 11},
+	{name: 'dQ',suit: 'd',rank: 'Q',value: 12},
+	{name: 'dK',suit: 'd',rank: 'K',value: 13},
+	{name: 'dA',suit: 'd',rank: 'A',value: 14}
+
+]
--- js/main.js
+++ js/main.js
@@ -1,138 +1,3 @@
-console.log('linked');
-
-
-
-var testDeck = {
-
-	cards: {
-
-		h2:{  
-		  name:'h2',
-		  suit:'h',
-		  value:2
-		},
-		h3:{  
-		  name:'h3',
-		  suit:'h',
-		  value:3
-		},
-		h4:{  
-		  name:'h4',
-		  suit:'h',
-		  value:4
-		},
-		h5:{  
-		  name:'h5',
-		  suit:'h',
-		  value:5
-		},
-		h6:{  
-		  name:'h6',
-		  suit:'h',
-		  value:6
-		},
-		h7:{  
-		  name:'h7',
-		  suit:'h',
-		  value:7
-		},
-		h8:{  
-		  name:'h8',
-		  suit:'h',
-		  value:8
-		},
-		h9:{  
-		  name:'h9',
-		  suit:'h',
-		  value:9
-		},
-		h10:{  
-		  name:'h10',
-		  suit:'h',
-		  value:10
-		},
-		hJ:{  
-		  name:'hJ',
-		  suit:'h',
-		  value:11
-		},
-		hQ:{  
-		  name:'hQ',
-		  suit:'h',
-		  value:12
-		},
-		hK:{  
-		  name:'hK',
-		  suit:'h',
-		  value:13
-		},
-		hA:{  
-		  name:'hA',
-		  suit:'h',
-		  value:14
-		}
-	}
-}
-
-var deck = [
-
-	{name: 'h2',suit: 'h',rank: '2',value: 2},
-	{name: 'h3',suit: 'h',rank: '3',value: 3},
-	{name: 'h4',suit: 'h',rank: '4',value: 4},
-	{name: 'h5',suit: 'h',rank: '5',value: 5},
-	{name: 'h6',suit: 'h',rank: '6',value: 6},
-	{name: 'h7',suit: 'h',rank: '7',value: 7},
-	{name: 'h8',suit: 'h',rank: '8',value: 8},
-	{name: 'h9',suit: 'h',rank: '9',value: 9},
-	{name: 'h10',suit: 'h',rank: '10',value: 10},
-	{name: 'hJ',suit: 'h',rank: 'J',value: 11},
-	{name: 'hQ',suit: 'h',rank: 'Q',value: 12},
-	{name: 'hK',suit: 'h',rank: 'K',value: 13},
-	{name: 'hA',suit: 'h',rank: 'A',value: 14},
-	{name: 'c2',suit: 'c',rank: '2',value: 2},
-	{name: 'c3',suit: 'c',rank: '3',value: 3},
-	{name: 'c4',suit: 'c',rank: '4',value: 4},
-	{name: 'c5',suit: 'c',rank: '5',value: 5},
-	{name: 'c6',suit: 'c',rank: '6',value: 6},
-	{name: 'c7',suit: 'c',rank: '7',value: 7},
-	{name: 'c8',suit: 'c',rank: '8',value: 8},
-	{name: 'c9',suit: 'c',rank: '9',value: 9},
-	{name: 'c10',suit: 'c',rank: '10',value: 10},
-	{name: 'cJ',suit: 'c',rank: 'J',value: 11},
-	{name: 'cQ',suit: 'c',rank: 'Q',value: 12},
-	{name: 'cK',suit: 'c',rank: 'K',value: 13},
-	{name: 'cA',suit: 'c',rank: 'A',value: 14},
-	{name: 's2',suit: 's',rank: '2',value: 2},
-	{name: 's3',suit: 's',rank: '3',value: 3},
-	{name: 's4',suit: 's',rank: '4',value: 4},
-	{name: 's5',suit: 's',rank: '5',value: 5},
-	{name: 's6',suit: 's',rank: '6',value: 6},
-	{name: 's7',suit: 's',rank: '7',value: 7},
-	{name: 's8',suit: 's',rank: '8',value: 8},
-	{name: 's9',suit: 's',rank: '9',value: 9},
-	{name: 's10',suit: 's',rank: '10',value: 10},
-	{name: 'sJ',suit: 's',rank: 'J',value: 11},
-	{name: 'sQ',suit: 's',rank: 'Q',value: 12},
-	{name: 'sK',suit: 's',rank: 'K',value: 13},
-	{name: 'sA',suit: 's',rank: 'A',value: 14},
-	{name: 'd2',suit: 'd',rank: '2',value: 2},
-	{name: 'd3',suit: 'd',rank: '3',value: 3},
-	{name: 'd4',suit: 'd',rank: '4',value: 4},
-	{name: 'd5',suit: 'd',rank: '5',value: 5},
-	{name: 'd6',suit: 'd',rank: '6',value: 6},
-	{name: 'd7',suit: 'd',rank: '7',value: 7},
-	{name: 'd8',suit: 'd',rank: '8',value: 8},
-	{name: 'd9',suit: 'd',rank: '9',value: 9},
-	{name: 'd10',suit: 'd',rank: '10',value: 10},
-	{name: 'dJ',suit: 'd',rank: 'J',value: 11},
-	{name: 'dQ',suit: 'd',rank: 'Q',value: 12},
-	{name: 'dK',suit: 'd',rank: 'K',value: 13},
-	{name: 'dA',suit: 'd',rank: 'A',value: 14}
-
-]
-
-
-
 $(function () {
 
 	var p1FaceDown =[];
@@ -160,21 +25,32 @@ $(function () {
 
 	p1cardSlots = [$p1cardSlot1, $p1cardSlot2, $p1cardSlot3];
 
-	var $player2HandElement = $('#p2-hand');
-	var $player1HandElement = $('#p1-hand');
+	var $p2HandElement = $('#p2-hand');
+	var $p1HandElement = $('#p1-hand');
 
 	var cardsInPlayIndexs = [];
 
 	var $p1ShowCards = $('#p1ShowCards');
 	var $p2ShowCards = $('#p2ShowCards');
 
+	var $p1Ready = $('#p1-ready');
+	var $p2Ready = $('#p2-ready');
+
+	var $p1CardSlots = $('#p1-card-slots');
+	var $p2CardSlots = $('#p2-card-slots');
+
+	var $p1SwapCards = $('#p1-swap-cards');
+	var $p2SwapCards = $('#p2-swap-cards');
+
+	var p1ValidSwap = false;
+	var p2ValidSwap = false;
+
+
 
 	function chooseRandomCard(){
 
 		var cardIsTaken = true;
 		var randomNumber = 0;
-
-		// debugger
 
 		if (cardsInPlayIndexs.length === 0) {
 
@@ -236,11 +112,27 @@ $(function () {
 
 	}
 
+	function getCardByName(cardNameString) {
 
-	function swapCardHandToFaceUpP1(cardFromHandIndex, cardFromFaceUpIndex){
+		for (var i = 0; i <deck.length; i++) {
 
-		var p1HandCard = p1Hand[cardFromHandIndex];
-		var p1FaceUpCard = p1FaceUp[cardFromFaceUpIndex];
+			if(deck[i].name === cardNameString){
+
+				return	deck[i];
+			}
+		}
+	}
+
+
+	function swapCardHandToFaceUpP1(cardFromHand, cardFromFaceUp){
+
+		var p1HandCard = getCardByName(cardFromHand);
+		var p1FaceUpCard = getCardByName(cardFromFaceUp);
+
+		debugger
+
+		cardFromHandIndex = p1Hand.indexOf(p1HandCard);
+		cardFromFaceUpIndex = p1FaceUp.indexOf(p1FaceUpCard);
 
 		p1Hand.splice(cardFromHandIndex,1);
 		p1FaceUp.splice(cardFromFaceUpIndex,1);
@@ -363,7 +255,7 @@ $(function () {
 
 			$handCardContainer.css('left',handLeftOffset + 'px');
 
-			$player2HandElement.append($handCardContainer);
+			$p2HandElement.append($handCardContainer);
 
 			cardCount++;
 		}
@@ -426,14 +318,14 @@ $(function () {
 
 			$handCardContainer.css('left',handLeftOffset + 'px');
 
-			$player1HandElement.append($handCardContainer);
+			$p1HandElement.append($handCardContainer);
 
 			cardCount++;
 		}
 
 	}
 
-	function p2ShowHand(){
+	function p2ToggleShowHand(){
 
 		var zIndex = $('.p2hand-face-up-card').css('z-index')
 
@@ -452,7 +344,7 @@ $(function () {
 
 	
 
-	function p1ShowHand(){
+	function p1ToggleShowHand(){
 
 		var zIndex = $('.p1hand-face-up-card').css('z-index')
 
@@ -506,33 +398,83 @@ $(function () {
 		// body...
 	}
 
+	function initalSetup() {
+
+		chooseFaceDownCards();
+		chooseFaceUpCards();
+		chooseHands();
+		
+		updateView();
+
+		p1ToggleShowHand();
+		p2ToggleShowHand();
+
+	}
+
+	function p1setupRound() {
+
+		$p1HandElement.on('click', '#p1-show-cards', p1ToggleShowHand);
+		$p1HandElement.on('click', '.p1hand-face-up-card', highlightCard);
+		$p1CardSlots.on('click', '.face-up-card', highlightCard);
+
+
+	}
+
+	function p2setupRound(){
+
+		$p2HandElement.on('click', '#p2-show-cards', p2ToggleShowHand);
+		$p2HandElement.on('click', '.p2hand-face-up-card', highlightCard);
+		$p2CardSlots.on('click', '.face-up-card', highlightCard);
+
+		$p1SwapCards.click(p1VerifyCardSwap)
+
+
+
+	}
+
+	function p1VerifyCardSwap() {
+
+		p1ValidSwap = $p1HandElement.find('.highlighted').length === $p1CardSlots.find('.highlighted').length;
+
+		if (p1ValidSwap) {
+
+			var $p2selectedHand = $p1HandElement.find('.highlighted');
+			var $p2selectedFaceUp = $p1CardSlots.find('.highlighted');
+
+			for (var i = 0; i < $p2selectedHand.length; i++) {
+
+				
+				swapCardHandToFaceUpP1($p2selectedHand.eq(i).data('name'),$p2selectedFaceUp.eq(i).data('name'));
+
+			}
+
+		}
+
+		updateP1View();
+
+		//can't swap more than once
+
+	}
+
+	function p2VerifyCardSwap() {
+
+		p2ValidSwap = $p1HandElement.find('.highlighted').length === $p1CardSlots.find('.highlighted').length;
+		debugger;
+
+	}
 
 
 
 
 
 
-	chooseFaceDownCards();
-	chooseFaceUpCards();
-	chooseHands();
-	chooseHands();
-	chooseHands();
-	chooseHands();
-	chooseHands();
-	chooseHands();
+
+	initalSetup();
+	p1setupRound();
+	$p1SwapCards.click(p1VerifyCardSwap)
+	
 
 	
-	updateView();
-
-	$player2HandElement.on('click', '#p2ShowCards', p2ShowHand);
-	$player1HandElement.on('click', '#p1ShowCards', p1ShowHand);
-
-	$player2HandElement.on('click', '.p2hand-face-up-card', highlightCard);
-	$player1HandElement.on('click', '.p1hand-face-up-card', highlightCard);
-
-
-
-
 
 	// swapCardHandToFaceUpP1(0,0);
 
